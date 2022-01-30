f3af44786d67ee3294c6affb1f80143cbe6185af
takeike <takemasa.ikeda@konicaminolta.com>
2022-01-30 20:27:43 +0900

    session32 task

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  //div
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //li
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+  // console.log(div);
+
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
