f4198fa1edc2f29cae1af69e4037e4c0bf0bd49b
takeike <takemasa.ikeda@konicaminolta.com>
2022-01-31 15:57:29 +0900

    section37 javascript app

--- src/index.js
+++ src/index.js
@@ -3,16 +3,70 @@ import "./styles.css";
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
+  createIncompleteList(inputText);
+};
+
+// 未完了リストから指定の要素を削除する
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+// 未完了リストに追加する
+const createIncompleteList = (text) => {
   //div
   const div = document.createElement("div");
   div.className = "list-row";
 
   //li
   const li = document.createElement("li");
-  li.innerText = inputText;
+  li.innerText = text;
+
+  // button生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    deleteFromIncompleteList(completeButton.parentNode);
+    //完了リストに追加するもの
+    const addTarget = completeButton.parentNode;
+    const text = addTarget.firstElementChild.innerText;
+
+    addTarget.textContent = null;
+
+    //li
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    //button
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      //親ダグを削除
+      const deleteTarget = backButton.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+
+      // text
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createIncompleteList(text);
+    });
+
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+
+    document.getElementById("complete-list").appendChild(addTarget);
+  });
+  // delete
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    //押された削除ボタンを削除する
+    deleteFromIncompleteList(deleteButton.parentNode);
+  });
 
   //divタグの子要素に各要素を設定
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
   // console.log(div);
 
   document.getElementById("incomplete-list").appendChild(div);
